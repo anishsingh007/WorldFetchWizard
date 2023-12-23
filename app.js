5bf26d1ab95e19b5399db4331d34d8a981b60760
anishsingh007 <emailanishsingh@gmail.com>
2023-12-23 21:25:33 +0530

    Routes created

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
+const authRoutes = require('./routes/authRoutes');
+const countryRoutes = require('./routes/countryRoutes')
 const app = express();
+
+app.use('/api', authRoutes); 
+app.use('/api', countryRoutes); 
+
+// Error handling for invalid routes
+app.use((req, res) => {
+    res.status(404).json({ error: "Not Found" });
+});
+
+// Error handling for general errors
+app.use((err, req, res, next) => {
+    console.error(err);
+    res.status(500).json({ error: "Internal Server Error" });
+});
 
 app.listen(3000, () => {
     console.log("Server started on port 3000");
---
+++ routes/authRoutes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+
+router.post('/auth', (req, res) => {
+   
+});
+
+module.exports = router;
+
